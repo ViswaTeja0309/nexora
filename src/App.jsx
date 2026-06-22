c87fd51328651542e821d7e1eb8b2aa7bc4d84e0
Gujjula Viswa Teja <viswatejagujjula@gmail.com>
2026-06-22 11:31:23 +0530

    Initial project setup with routing and folder structure

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Eligibility from './pages/Eligibility'
+import Result from './pages/Result'
+import Profile from './pages/Profile'
+
+
+const App = () => {
+
+  return (
+
+    <>
+    
+    <BrowserRouter>
+
+      <Routes>
+
+          <Route path = '/' element={<Home/>}/>
+          <Route path = '/dashboard' element={<Dashboard/>}/>
+          <Route path = '/login' element={<Login/>}/>
+          <Route path = '/signup' element={<Signup/>}/>
+          <Route path = '/eligibilty' element={<Eligibility/>}/>
+          <Route path = '/result' element={<Result/>}/>
+          <Route path = '/profile' element={<Profile/>}/>
+
+      </Routes>
+      
+    </BrowserRouter>
+
+    
+    </>
+  )
+}
+
+export default App
+
+
